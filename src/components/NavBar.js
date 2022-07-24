cc2e1ea966e622cc1e1992c0c761047b1d903838
Kuan Hui, Wu <tf00604520@gmail.com>
2022-07-24 11:25:39 +0800

    change favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,10 @@
 const NavBar = () => {
   return (
     <nav id="navbar" className="black-theme-block">
-      {/* <img src="" alt="logo"></img> */}
       <div id="navbar-item-container" className="flex-start">
 
         <div className="navbar-item">
-          <div href="#" className="navbar-item-link">
+          <div className="navbar-item-link">
             仕彩有限公司
           </div>
         </div>
