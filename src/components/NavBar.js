5e2878b2c550182e93c83bf8fd14c2a94d1b94d7
KuanHui <kuanhui.dev@gmail.com>
2022-07-26 11:32:24 +0800

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import icon from "../static/image/w9999gold-icon.png";
 const NavBar = () => {
   return (
     <nav id="navbar" className="black-theme-block">
-
       <div id="navbar-items-start" className="flex-start">
         <div className="navbar-item">
           <img src={icon} alt="icon" width="50" height="24.33"></img>
@@ -16,11 +15,10 @@ const NavBar = () => {
       <div id="navbar-items-second">
         <div className="navbar-item">
           <div className="navbar-item-link">
-            <button onClick={handleClick}>icon</button>
+            <button>icon</button>
           </div>
         </div>
       </div>
-
     </nav>
   );
 };
