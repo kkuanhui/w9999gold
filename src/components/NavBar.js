53c8052d28b7ae8814b3675d3a4646533126019b
KuanHui <kuanhui.dev@gmail.com>
2022-07-26 16:03:11 +0800

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,43 @@
 import icon from "../static/image/w9999gold-icon.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isPopup, setIsPopup] = useState(false);
+
   return (
     <nav id="navbar" className="black-theme-block">
-      <div id="navbar-items-start" className="flex-start">
-        <div className="navbar-item">
-          <img src={icon} alt="icon" width="50" height="24.33"></img>
-        </div>
-        <div className="navbar-item">
-          <div className="navbar-item-link">仕彩金飾</div>
-        </div>
+      <div className="navbar-block flex-start">
+        <img src={icon} alt="icon" width="50" height="24.33"></img>
+        <div>仕彩金飾</div>
       </div>
+      <div className="navbar-block">
+        <div id="navbar-hamburger">
+          <button
+            onClick={() => {
+              setIsPopup(!isPopup);
+            }}
+          >
+            <GiHamburgerMenu />
+          </button>
+        </div>
 
-      <div id="navbar-items-second">
-        <div className="navbar-item">
-          <div className="navbar-item-link">
-            <button>icon</button>
+        <div
+          id="navbar-collapse"
+          onClick={() => {
+            setIsPopup(!isPopup);
+          }}
+          style={{ display: isPopup ? "block" : "none" }}
+        >
+          <div>
+            <ul id="navbar-link-list">
+              <li>
+                <a>產品價格</a>
+              </li>
+              <li>
+                <a>黃金市價</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,12 @@
-
-import { Fragment } from 'react';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import AboutUsBlock from './AboutUsBlock';
-import PriceBlock from './PriceBlock.js';
-import Popup from "../components/Popup.js"
+import { Fragment, useState } from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import AboutUsBlock from "./AboutUsBlock";
+import PriceBlock from "./PriceBlock.js";
 
 const App = () => {
-
   return (
     <Fragment>
-
       <div id="main-content">
         <NavBar />
         <PriceBlock />
@@ -18,12 +14,8 @@ const App = () => {
       </div>
 
       <Footer></Footer>
-
-      <Popup></Popup>
-
     </Fragment>
   );
-}
-
+};
 
 export default App;
