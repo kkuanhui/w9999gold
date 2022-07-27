182890e38ebfdacfd1fb38dc663085412794657b
KuanHui <kuanhui.dev@gmail.com>
2022-07-27 18:20:40 +0800

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,30 @@
 import icon from "../static/image/w9999gold-icon.png";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 
 const NavBar = () => {
   const [isPopup, setIsPopup] = useState(false);
   return (
     <nav id="navbar" className="black-theme-block">
-
-      <div className="navbar-block flex-start">
+      <div className="navbar-block" style={{ display: "flex" }}>
         <img src={icon} alt="icon" width="50" height="24.33"></img>
         <div>仕彩金飾</div>
       </div>
 
       <div className="navbar-block">
-
-        <div id="navbar-hamburger">
-          <button
-            onClick={() => {
-              setIsPopup(!isPopup);
-            }}
-          >
-            <GiHamburgerMenu />
-          </button>
+        <label id="navbar-hamburger" for="navbar-toggler">
+          &#9776;
+        </label>
+        <input type="checkbox" id="navbar-toggler"></input>
+        <div id="navbar-list">
+          <ul>
+            <li>
+              <a href="/price">產品價格</a>
+            </li>
+            <li>
+              <a href="/market">黃金市價</a>
+            </li>
+          </ul>
         </div>
-
-        <div
-          id="navbar-collapse"
-          onClick={() => {
-            setIsPopup(!isPopup);
-          }}
-          style={{ display: isPopup ? "block" : "none" }}
-        >
-          <div>
-            <ul id="navbar-link-list">
-              <li>
-                <a href="/price">產品價格</a>
-              </li>
-              <li>
-                <a href="/market">黃金市價</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
       </div>
     </nav>
   );
--- src/components/PriceDetail.js
+++ src/components/PriceDetail.js
@@ -1,67 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Flickity from "react-flickity-component";
-import '../static/css/flickity.css';
-import PriceTable from './PriceTable';
+import "../static/css/flickity.css";
+import PriceTable from "./PriceTable";
 
 // import axios from 'axios';
 
 const PriceDetail = () => {
-
-	const flickityOptions = {
-		initialIndex: 0,
-		wrapAround: true,
-        pageDots: false,
-	}
+  const flickityOptions = {
+    initialIndex: 0,
+    wrapAround: true,
+    pageDots: false
+  };
 
   const [Data, setData] = useState([]);
-	
-  // Second argument of useEffect is an enpty array [], which makes it only invoked at the first render.
-  useEffect(
-    () => {
-    fetch('https://w9999gold-backend.herokuapp.com/backend-1')
-    .then(res => 
-        res.json()
-      ).then(data => 
-      setData(data)
-      )
-    .catch(err => 
-      console.log('Something goes wrong.\n', err
-      ))
-    }, 
-    [])
-  
-	return(
 
-     <div id="price-detail" className=''>
+  // Second argument of useEffect is an enpty array [], which makes it only invoked at the first render.
+  useEffect(() => {
+    fetch("https://w9999gold-backend.herokuapp.com/backend-1")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log("Something goes wrong.\n", err));
+  }, []);
+
+  return (
+    <div id="price-detail">
       <Flickity
-        id={''}
-        className={'price-detail-element'} // default ''
+        id={""}
+        className={"price-detail-element"} // default ''
         style={{}}
-        elementType={'div'} // default 'div'
+        elementType={"div"} // default 'div'
         options={flickityOptions} // takes flickity options {}
         disableImagesLoaded={false} // default false
         reloadOnUpdate // default false
-        static// default false
+        static // default false
       >
-
-      {
-        // React require loop item must contains key. So I have to add index on each one.
-        Data.map( (item, idx) => {
-          return (
-            <PriceTable 
-              key={idx}
-              futurePrice = {item.futurePrice}
-              sizeTraditional = {item.sizeTraditional}
-              sizeCM = {item.sizeCM}
-              initialWeight = {Number(item.initialWeight)}
-              maxWeight = {item.maxWeight}
-              wage = {item.wage}
-            /> 
-          )
-        })
-      }
-
-      </ Flickity>
+        {
+          // React require loop item must contains key. So I have to add index on each one.
+          Data.map((item, idx) => {
+            return (
+              <PriceTable
+                key={idx}
+                futurePrice={item.futurePrice}
+                sizeTraditional={item.sizeTraditional}
+                sizeCM={item.sizeCM}
+                initialWeight={Number(item.initialWeight)}
+                maxWeight={item.maxWeight}
+                wage={item.wage}
+              />
+            );
+          })
+        }
+      </Flickity>
 
       <div id="price-detail-info" className="price-detail-element">
         <p>以上表格僅顯示常規產品參考價格，更大尺寸歡迎詢問。</p>
@@ -69,8 +58,7 @@ const PriceDetail = () => {
         <p>最終售價以本公司專員報價為準。</p>
       </div>
     </div>
-
-    )
-}
+  );
+};
 
 export default PriceDetail;
