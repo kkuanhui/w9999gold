e9b6ca6a9570b0aa82fd5507905e51b18ea81cf1
Kuan Hui, Wu <tf00604520@gmail.com>
2022-09-07 23:26:55 +0800

    ui init

--- src/components/AppProduct.js
+++ src/components/AppProduct.js
@@ -26,11 +26,37 @@ const ProductOptions = () => {
     <div id="product-options">
       <div className="flex-start">
         <div>金牌設計</div>
-        <div className="select">
+        <select>
+          <option value="E3456">龍鳳龍鳳</option>
+          <option value="E3456">雙龍搶珠</option>
+        </select>
+      </div>
+      <div className="flex-start">
+        <div>金牌尺寸</div>
+        <select>
+          <option value="E3456">2 寸</option>
+          <option value="E3456">2 寸</option>
+        </select>
+      </div>
+      <div className="flex-start">
+        <div>黃金重量</div>
+        <select>
+          <option value="E3456">2 寸</option>
+          <option value="E3456">2 寸</option>
+        </select>
+        <div>時價</div>
+        <div>3,650</div>
+      </div>
+      <div className="flex-start">
+        <div>增加照片</div>
+        <input type="checkbox"></input>
+      </div>
+      <div className="flex-start">
+        <div>增加外框</div>
           <select>
-            <option value="E3456">雙龍搶珠</option>
+            <option value="E3456">龍鳳搶珠</option>
+            <option value="E3456">雙龍雙龍</option>
           </select>
-        </div>
       </div>
     </div>
   )
