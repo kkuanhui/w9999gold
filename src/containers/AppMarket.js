f18a6b9273aa72b59111daeeafba809e78f99216
Kuan Hui, Wu <tf00604520@gmail.com>
2022-09-19 00:57:40 +0800

    init App market price

---
+++ src/containers/AppMarket.js
@@ -0,0 +1,13 @@
+import MarketPriceChart from "../components/MarketPriceChart"
+
+const AppMarket = () => {
+  return (
+    <div>
+      <div>This is App Market</div>
+      <div>head lines</div>
+      <MarketPriceChart></MarketPriceChart>
+    </div>
+  )
+}
+
+export default AppMarket
