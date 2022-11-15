a90253db5505d550a57c8b3a75e1d2a8780f0bf2
kuanhui.wu <kuanhui.wu@innolux.com>
2022-11-15 17:00:50 +0800

    update

--- src/ProductGoldfan.js
+++ src/ProductGoldfan.js
@@ -1,39 +1,94 @@
 import React, {useRef, useEffect, useState} from "react"
+import axios from "axios";
 
 const ProductGoldfan = () => {
-  return(
+  const [goldPrice, setGoldPrice] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [isCustom, setIsCustom] = useState(false);
+  const [productId, setProductId] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const requestGoldPrice = () => {
+    axios.get(`/get-gold-quote`).then((res) => {
+      setGoldPrice(res.data[0]["price_value"]);
+    });
+    setTimeout(requestGoldPrice, 1000 * 60 * 5)
+  }
+  useEffect(requestGoldPrice, []);
+
+  useEffect(() => {
+    axios.get(`/list-products/A03`).then((res) => {
+      setProductList(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const customFee = isCustom? productId["wage_text_custom"]: 0
+    const goldFee = goldPrice * 0.1
+    console.log(customFee, goldFee)
+    setTotalPrice(goldFee + customFee)
+  }, [isCustom, goldPrice, productId])
+
+
+  const handleChange = (event) => {
+    axios.get(`/get-detail/A03/${event.target.value}`).then((res) => {
+      setProductId(res.data[0]);
+      console.log(res.data[0])
+    })
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    axios.get(`/get-detail/A03/P0301`).then((res) => {
+      setProductId(res.data[0]);
+      console.log(res.data[0])
+    })
+  }, [])
+
+  const handleChecked = () => {
+    setIsCustom(!isCustom)
+  }
+
+  return (
     <div>
-      <h1>黃金扇</h1>
-      <div className="flex-center" style={{"margin": "10px", "gap": "10px", "flexDirection": "column"}}>
-        <div id="product-options">
-          <div className="product-attr-choice">
-            <div>黃金扇設計</div>
-          </div>
+      <h1 className="width-70 text-center">GOLDFAN</h1>
 
-          <div className="product-attr-choice">
-            <div>
-              <div>黃金重量</div>
-              <div >0.1</div>
-            </div>
-            <div className="extend-text">
-              <div>時價</div>
-              <div>3,650</div>
-            </div>
-          </div>
+      <div
+        className="d-grid grid-column-3 mr-auto ml-auto width-50"
+        style={{ gridTemplateColumns: "3fr 2fr 1fr 1fr" }}
+      >
 
-          <div className="product-attr-choice">
-            <div>增加文字</div>
-            <input type="checkbox"></input>
-          </div>
-
+        <div>御守設計</div>
+        <div>
+          <select onChange={handleChange}>
+            {productList.map((ele) => {
+              return (
+                <option value={ele["product_id"]}>{ele["show_name"]}</option>
+              );
+            })}
+          </select>
         </div>
+
+        <div style={{ gridRow: "2" }}>黃金重量</div>
+        <div style={{ gridRow: "2" }}>0.1 錢</div>
+        <div style={{gridArea: "2/3"}}>時價</div>
+        <div style={{ gridArea: "2/4" }} className="d-flex">
+          <div>{Number(goldPrice * 0.1).toFixed()}</div>
+        </div>
+
+        <div style={{ gridRow: "3" }}>增加文字</div>
+        <div style={{ gridRow: "3" }}>
+          <input type="checkbox" onClick={handleChecked} checked={isCustom}></input>
+        </div>
+
+        <div style={{ gridArea: "4/1/5/5", borderTop: "1px solid #3f3f3f"}}></div>
+
+        <div style={{ gridRow: "5" }}>總價</div>
+        <div style={{ gridRow: "5" }}>$<span>{totalPrice.toFixed()}</span></div>
+
       </div>
     </div>
-  )
-}
-
-
-// We are using useRef to manipulate <canvas> in React.js.
-// 我只要先宣告一個空的 useRef()，然後當成 dom 上 ref attribute 的值，這時候再元件任何地方印出 canvasRef.current 就會是這個 DOM
+  );
+};
 
 export default ProductGoldfan;
--- src/ProductOmori.js
+++ src/ProductOmori.js
@@ -1,95 +1,94 @@
-import axios from "axios"
-import React, {useRef, useEffect, useState} from "react"
+import axios from "axios";
+import React, { useRef, useEffect, useState } from "react";
 
 const ProductOmori = () => {
+  const [goldPrice, setGoldPrice] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [isCustom, setIsCustom] = useState(false);
+  const [productId, setProductId] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const [goldPrice, setGoldPrice] = useState([])
-  const [productList, setProductList] = useState([])
-  const [goldWeight, setGoldWeight] = useState(0.1)
-  const [isCustom, setIsCustom] = useState(false)
-  const [productId, setProductId] = useState('')
-  const [totalPrice, setTotalPrice] = useState(0)
-
-  const sevenMin = 1000 * 60 * 7
-  let fifteenCheck = 0
-
-  setInterval(() => {
-    fifteenCheck++    
-  }, sevenMin)
+  const requestGoldPrice = () => {
+    axios.get(`/get-gold-quote`).then((res) => {
+      setGoldPrice(res.data[0]["price_value"]);
+    });
+    setTimeout(requestGoldPrice, 1000 * 60 * 5)
+  }
+  useEffect(requestGoldPrice, []);
 
   useEffect(() => {
-    axios.get(`/get-gold-quote`)
-    .then((res) => {
-      setGoldPrice(res.data[0])
+    axios.get(`/list-products/A04`).then((res) => {
+      setProductList(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const customFee = isCustom? productId["wage_text_custom"]: 0
+    const goldFee = goldPrice * 0.1
+    console.log(customFee, goldFee)
+    setTotalPrice(goldFee + customFee)
+  }, [isCustom, goldPrice, productId])
+
+
+  const handleChange = (event) => {
+    axios.get(`/get-detail/A04/${event.target.value}`).then((res) => {
+      setProductId(res.data[0]);
+      console.log(res.data[0])
     })
-  }, fifteenCheck)
+    .catch(err => console.log(err))
+  }
 
   useEffect(() => {
-    axios.get(`/list-products/A04`)
-    .then(res => {
-      setProductList(res.data)
+    axios.get(`/get-detail/A04/P0401`).then((res) => {
+      setProductId(res.data[0]);
+      console.log(res.data[0])
     })
   }, [])
 
-  useEffect(() => {
-    axios.get(`/get-detail/A04/${productId}`)
-    .then(res => {
-      setProductId(res.data)
-    })
-  }, productId)
+  const handleChecked = () => {
+    setIsCustom(!isCustom)
+  }
 
   return (
     <div>
-      <h1 className="width-70">
-        黃金御守
-      </h1>
+      <h1 className="width-70 text-center">OMORI</h1>
+
       <div
-        className="flex-center"
-        style={{ margin: "10px", gap: "10px", flexDirection: "column" }}
+        className="d-grid grid-column-3 mr-auto ml-auto width-50"
+        style={{ gridTemplateColumns: "3fr 2fr 1fr 1fr" }}
       >
-        <div id="product-options">
-          <div className="product-attr-choice">
-            <div>御守設計</div>
-            <select>
-              {productList.map(ele => {
-                return <option value={ele["product_id"]}>{ele["show_name"]}</option>
-              })}
-            </select>
-          </div>
 
-          <div className="product-attr-choice">
-            <div>
-              <div>黃金重量</div>
-              <div>0.1 錢</div>
-            </div>
-            <div className="extend-text">
-              <div>時價</div>
-              <div>{Number(goldPrice["price_value"] * 0.1).toFixed()}</div>
-            </div>
-          </div>
-
-          <div className="product-attr-choice">
-            <div>增加文字</div>
-            <input type="checkbox"></input>
-          </div>
-
-          <div>--------------</div>
-
-          <div>
-            <div>總價</div>
-            <div>$<span>{totalPrice}</span></div>
-          </div>
-
-
+        <div>御守設計</div>
+        <div>
+          <select onChange={handleChange}>
+            {productList.map((ele) => {
+              return (
+                <option value={ele["product_id"]}>{ele["show_name"]}</option>
+              );
+            })}
+          </select>
         </div>
-      </div>
 
+        <div style={{ gridRow: "2" }}>黃金重量</div>
+        <div style={{ gridRow: "2" }}>0.1 錢</div>
+        <div style={{gridArea: "2/3"}}>時價</div>
+        <div style={{ gridArea: "2/4" }} className="d-flex">
+          <div>{Number(goldPrice * 0.1).toFixed()}</div>
+        </div>
+
+        <div style={{ gridRow: "3" }}>增加文字</div>
+        <div style={{ gridRow: "3" }}>
+          <input type="checkbox" onClick={handleChecked} checked={isCustom}></input>
+        </div>
+
+        <div style={{ gridArea: "4/1/5/5", borderTop: "1px solid #3f3f3f"}}></div>
+
+        <div style={{ gridRow: "5" }}>總價</div>
+        <div style={{ gridRow: "5" }}>$<span>{totalPrice.toFixed()}</span></div>
+
+      </div>
     </div>
   );
-}
-
-
-// We are using useRef to manipulate <canvas> in React.js.
-// 我只要先宣告一個空的 useRef()，然後當成 dom 上 ref attribute 的值，這時候再元件任何地方印出 canvasRef.current 就會是這個 DOM
+};
 
 export default ProductOmori;
