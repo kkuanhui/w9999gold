7cfa32dd88eb89850bd1164483c7a55feffdf424
Kuan Hui, Wu <tf00604520@gmail.com>
2022-11-27 10:33:25 +0800

    update

--- src/ProductOmori.js
+++ src/ProductOmori.js
@@ -1,91 +1,98 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useRef, useEffect, useState } from "react";
+import {filter} from "lodash";
 
 const ProductOmori = () => {
   const [goldPrice, setGoldPrice] = useState([]);
-  const [productList, setProductList] = useState([]);
-  const [isCustom, setIsCustom] = useState(false);
-  const [productId, setProductId] = useState("");
+  const [productDetails, setProductDetails] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const [userProduct, setUserProduct] = useState("");
+  const [userWeight, setUserWeight] = useState(0);
+  const [isAddText, setIsAddText] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
 
-  const requestGoldPrice = () => {
-    axios.get(`/get-gold-quote`).then((res) => {
-      setGoldPrice(res.data[0]["price_value"]);
-    });
-    setTimeout(requestGoldPrice, 1000 * 60 * 5)
-  }
-  useEffect(requestGoldPrice, []);
-
   useEffect(() => {
-    axios.get(`/list-products/A04`).then((res) => {
-      setProductList(res.data);
-    });
+    axios
+      .all([
+        axios.get(`/get-gold-quote`),
+        axios.get(`/list-products/A04`),
+        axios.get(`/get-detail/A04`),
+      ])
+      .then(
+        axios.spread((...res) => {
+          setGoldPrice(res[0].data);
+          setProducts(res[1].data);
+          setProductDetails(res[2].data);
+          const firstDetail = res[2].data[1];
+          setUserProduct(firstDetail["product_id"]);
+          setUserWeight(firstDetail["weight"]);
+        })
+      )
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   useEffect(() => {
-    const customFee = isCustom? productId["wage_text_custom"]: 0
-    const goldFee = goldPrice * 0.1
-    console.log(customFee, goldFee)
-    setTotalPrice(goldFee + customFee)
-  }, [isCustom, goldPrice, productId])
-
-
-  const handleChange = (event) => {
-    axios.get(`/get-detail/A04/${event.target.value}`).then((res) => {
-      setProductId(res.data[0]);
-      console.log(res.data[0])
-    })
-    .catch(err => console.log(err))
-  }
+    const details = filter(productDetails, {
+      "product_id": userProduct,
+    });
+  }, [userProduct])
 
   useEffect(() => {
-    axios.get(`/get-detail/A04/P0401`).then((res) => {
-      setProductId(res.data[0]);
-      console.log(res.data[0])
-    })
-  }, [])
+    const quote = goldPrice[0]?.["price_value"];
+    const partGold = quote * userWeight;
+    const detail = filter(productDetails, {
+      "product_id": userProduct,
+    });
+    const partCustom = isAddText ? detail[0]?.["wage_text_custom"] : 0;
+    setTotalPrice(partGold + partCustom);
+  }, [userProduct, userWeight, isAddText]);
 
-  const handleChecked = () => {
-    setIsCustom(!isCustom)
-  }
 
   return (
     <div>
       <h1 className="width-70 text-center">黃金御守</h1>
 
       <div
-        className="d-grid grid-column-3 mr-auto ml-auto width-50"
-        style={{ gridTemplateColumns: "3fr 2fr 1fr 1fr" }}
+        className="d-grid mr-auto ml-auto width-50"
+        style={{ gridTemplateColumns: "2fr 3fr" }}
       >
+        <div>產品設計</div>
+        <select
+          value={userProduct}
+          onChange={(e) => {
+            setUserProduct(e.target.value);
+          }}
+        >
+          {products.map((ele) => {
+            return (
+              <option value={ele["product_id"]}>{ele["show_name"]}</option>
+            );
+          })}
+        </select>
 
-        <div>御守設計</div>
-        <div>
-          <select onChange={handleChange}>
-            {productList.map((ele) => {
-              return (
-                <option value={ele["product_id"]}>{ele["show_name"]}</option>
-              );
-            })}
-          </select>
+        <div style={{}}>黃金重量</div>
+        <div style={{}}>{userWeight} 錢</div>
+        <div style={{}}>時價</div>
+        <div style={{}} className="d-flex">
+          <div>{(goldPrice[0]?.["price_value"] * userWeight).toFixed()}元</div>
         </div>
 
-        <div style={{ gridRow: "2" }}>黃金重量</div>
-        <div style={{ gridRow: "2" }}>0.1 錢</div>
-        <div style={{gridArea: "2/3"}}>時價</div>
-        <div style={{ gridArea: "2/4" }} className="d-flex">
-          <div>{Number(goldPrice * 0.1).toFixed()}</div>
+        <div style={{}}>增加文字</div>
+        <div style={{}}>
+          <input
+            type="checkbox"
+            onClick={() => setIsAddText(!isAddText)}
+            checked={isAddText}
+          ></input>
         </div>
 
-        <div style={{ gridRow: "3" }}>增加文字</div>
-        <div style={{ gridRow: "3" }}>
-          <input type="checkbox" onClick={handleChecked} checked={isCustom}></input>
+        <div style={{}}>總價</div>
+        <div style={{}}>
+          $<span>{totalPrice.toFixed()}元</span>
         </div>
-
-        <div style={{ gridArea: "4/1/5/5", borderTop: "1px solid #3f3f3f"}}></div>
-
-        <div style={{ gridRow: "5" }}>總價</div>
-        <div style={{ gridRow: "5" }}>$<span>{totalPrice.toFixed()}</span></div>
-
       </div>
     </div>
   );
