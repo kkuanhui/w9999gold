5943373a9b60808e83504a86e093f37e51bef9e1
kuanhui.wu <kuanhui.wu@innolux.com>
2022-11-29 15:20:30 +0800

    add image

--- src/ProductCards.js
+++ src/ProductCards.js
@@ -7,7 +7,7 @@ const Card = (props) => {
   return (
     <div>
       <Link to={props.linkTarget}>
-        <img src={""} width="250" height="200"></img>
+        <img src={props.imgUrl} width="250" height="200" alt={'card img'}></img>
         <div>{props.name}</div>
       </Link>
     </div>
@@ -22,13 +22,18 @@ const ProductCards = (props) => {
       setApps(res.data)
     })
   }, [])
+  const imgUrls = [
+    "https://res.cloudinary.com/hbehita9k/image/upload/v1669652312/w9999gold/plate-deities-1_tunbky.png",
+    "https://res.cloudinary.com/hbehita9k/image/upload/v1669652316/w9999gold/plate-deities-2_wjy9xw.png",
+    "https://res.cloudinary.com/hbehita9k/image/upload/v1669652321/w9999gold/goldfan-1.png",
+    "https://res.cloudinary.com/hbehita9k/image/upload/v1669652318/w9999gold/omori-1_ichsxz.png"
+  ]
   return (
     <div id="app-product">
-      {
-        apps.map(obj => {
-          return <Card linkTarget={obj['english_name']} name={obj['show_name']}></Card>
-        })
-      }
+      <Card linkTarget={apps[0]?.['english_name']} name={apps[0]?.['show_name']} imgUrl={imgUrls[0]}></Card>
+      <Card linkTarget={apps[1]?.['english_name']} name={apps[1]?.['show_name']} imgUrl={imgUrls[1]}></Card>
+      <Card linkTarget={apps[2]?.['english_name']} name={apps[2]?.['show_name']} imgUrl={imgUrls[2]}></Card>
+      <Card linkTarget={apps[3]?.['english_name']} name={apps[3]?.['show_name']} imgUrl={imgUrls[3]}></Card>
     </div>
   );
 };
