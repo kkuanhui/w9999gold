2a345a1fea84c556691a0112c07c0dd0f3d7c1cc
Kuan Hui, Wu <tf00604520@gmail.com>
2022-12-02 00:36:50 +0800

    alter jumbotron

--- src/HomeJumbotron.js
+++ src/HomeJumbotron.js
@@ -4,7 +4,9 @@ import image from './static/image/j-1.jpg'
 const HomeJumbotron = (props) => {
   return (
     <div className="jumbotron">
-      <img className="width-100 height-100" style={{objectFit: 'cover'}} src={image} alt="jumbotron" width='400'></img>
+      <div>
+        <img className="width-100 height-100" style={{objectFit: 'cover'}} src={image} alt="jumbotron" width='400'></img>
+      </div>
     </div>
   );
 };
