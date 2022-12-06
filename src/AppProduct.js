17d8a01e14402d944dfdef6df6a8308c968b8afc
Kuan Hui, Wu <tf00604520@gmail.com>
2022-12-06 23:37:26 +0800

    minify

--- src/AppProduct.js
+++ src/AppProduct.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {lazy} from "react";
 import { Routes, Route } from "react-router-dom";
-import ProductCards from "./ProductCards";
-import ProductPlateDeities from "./ProductPlateDeities";
-import ProductPlateCreativity from "./ProductPlateCreativity";
-import ProductGoldfan from "./ProductGoldfan";
-import ProductOmori from "./ProductOmori";
+
+// import ProductPlateDeities from "./ProductPlateDeities";
+// import ProductPlateCreativity from "./ProductPlateCreativity";
+// import ProductGoldfan from "./ProductGoldfan";
+// import ProductOmori from "./ProductOmori";
 
 // 1.1 plate_deities 神明金牌
 // 1.2 plate_creativity 創意金牌
 // 2.  omori 御守
 // 3.  goldfan 黃金扇
+
+const ProductCards = lazy(() => import('./ProductCards'));
+const ProductPlateCreativity = lazy(() => import('./ProductPlateCreativity'));
+const ProductPlateDeities = lazy(() => import('./ProductPlateDeities'));
+const ProductOmori = lazy(() => import('./ProductOmori'));
+const ProductGoldfan = lazy(() => import('./ProductGoldfan'));
 
 const AppProduct = () => {
   return (
