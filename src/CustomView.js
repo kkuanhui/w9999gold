3b3fe32c3a9f2af89b469c3b8c2abd900a57c6f8
kuanhui.wu <kuanhui.wu@innolux.com>
2023-05-15 16:59:45 +0800

    update

--- src/CustomView.js
+++ src/CustomView.js
@@ -1,16 +1,22 @@
-import React, {useState, useRef} from 'react'
-import { AiOutlineZoomIn, AiOutlineZoomOut } from 'react-icons/ai'
+import React, {useState, useRef, useEffect} from 'react'
+import { AiOutlineFontSize, AiOutlineZoomIn, AiOutlineZoomOut } from 'react-icons/ai'
 import './static/css/custom-view.css'
 
 const CustomView = () => {
   const [zoomIndex, setZoomIndex] = useState(1)
+  const [choosedItem, setChoosedItem] = useState(null)
   const dom = useRef(null)
   const container = useRef(null)
+
+  useEffect(() => {
+    window.onclick = (e) => {   
+      setChoosedItem(e.target.getAttribute("idx"))
+    };
+  })
 
   const onDoubleClick = (e) => {
     dom.current.style.overflow = 'visible'
   }
-
   const zoom = (sign) => {
     if(sign === "in"){
       setZoomIndex(zoomIndex+0.1)
@@ -20,7 +26,6 @@ const CustomView = () => {
       container.current.style.scale = `${zoomIndex - 0.1}`
     }
   }
-
   return(
     <div
       className="mb-3"
@@ -40,7 +45,9 @@ const CustomView = () => {
         <button onClick={() => {zoom('out')}}><AiOutlineZoomOut/></button>
       </div>
       <div ref={container} id="custom-view-container">
-        <CustomViewItem zoomIndex={zoomIndex}></CustomViewItem>
+        <CustomViewItem idx="1" choosedItem={choosedItem}></CustomViewItem>
+        <CustomViewItem idx="2" choosedItem={choosedItem}></CustomViewItem>
+        <CustomViewItem idx="3" choosedItem={choosedItem}></CustomViewItem>
       </div>
     </div>
   )
@@ -52,59 +59,62 @@ const CustomViewItem = (props) => {
 
   const myContainer = useRef(null)
   const myContent = useRef(null)
+  const [isclicked, setIsclicked] = useState('false')
+
+  useEffect(() => {
+    if(props.idx === props.choosedItem){
+      setIsclicked('true')
+    }else{
+      setIsclicked('false')
+    }
+  }, [props.choosedItem, props.idx])
 
   const onDrag = (e) => {
     let pos11 = 0, pos12 = 0, pos21 = 0, pos22 = 0 
-    console.log('key down')
     pos11 = e.clientX
     pos12 = e.clientY
-    myContainer.current.style.cursor  = `grab`
     document.onmousemove = (e) => {
       pos21 = e.clientX - pos11
       pos22 = e.clientY - pos12
       pos11 = e.clientX
       pos12 = e.clientY
-      myContainer.current.style.left = `${myContainer.current.offsetLeft + (pos21/props.zoomIndex)}px`
-      myContainer.current.style.top  = `${myContainer.current.offsetTop + (pos22/props.zoomIndex)}px`
-      myContainer.current.style.cursor  = `grabbing`
+      myContainer.current.style.left = `${myContainer.current.offsetLeft + pos21}px`
+      myContainer.current.style.top  = `${myContainer.current.offsetTop  + pos22}px`
     }
     document.onmouseup = (e) => {
       document.onmousemove = null
-      myContainer.current.style.border = null
-      myContainer.current.style.cursor  = `default`
     }
   } 
 
   const onReWidth = (e) => {
     let pos1 = 0, pos2 = 0
-    console.log('re width', e)
     pos1 = e.clientX
     // first position
     document.onmousemove = (e) => {
       pos2 = e.clientX - pos1 //movement
       pos1 = e.clientX
-      myContainer.current.style.offsetWidth = `${myContainer.current.offsetWidth - pos2/props.zoomIndex}px`
+      myContainer.current.style.width = `${myContainer.current.offsetWidth + pos2}px`
     } 
     document.onmouseup = () => {
-
+      document.onmousemove = null
     }
   }
 
   const onResize = (e) => {
-    let pos11 = 0, pos12 = 0, pos21 = 0, pos22 = 0 
+    let pos11 = 0, pos12 = 0, pos21 = 0, pos22 = 0
     pos11 = e.clientX
     pos12 = e.clientY
-    document.querySelector('#custom-view').style.overflow = 'visible'
+    // first position
     document.onmousemove = (e) => {
-      pos21 = e.clientX - pos11
-      pos22 = e.clientY - pos12
+      pos21 = e.clientX - pos11 //movement
+      pos22 = e.clientY - pos12 //movement
       pos11 = e.clientX
       pos12 = e.clientY
-      myContainer.current.style.left = `${myContainer.current.offsetLeft + (pos21/props.zoomIndex)}px`
-      myContainer.current.style.top  = `${myContainer.current.offsetTop + (pos22/props.zoomIndex)}px`
-    }
+      myContainer.current.style.width  = `${myContainer.current.offsetWidth  + pos21}px`
+      myContainer.current.style.height = `${myContainer.current.offsetHeight + pos22}px`
+    } 
     document.onmouseup = () => {
-
+      document.onmousemove = null
     }
   } 
 
@@ -112,6 +122,8 @@ const CustomViewItem = (props) => {
   return(
     <div 
       ref={myContainer}
+      isclicked={isclicked}
+      idx={props.idx}
       className='custom-view-item'
       style={{
         position: "absolute", 
@@ -136,30 +148,38 @@ const CustomViewItem = (props) => {
 
       <div 
         onMouseDown={(e) => {onResize(e)}}
-        className="resize-border resize-dot" 
+        className="resize-dot" 
         style={{cursor: "nw-resize", top: "0px", left: "0px", transform: "translate(-50%, -50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
-        className="resize-border resize-dot" 
+        className="resize-dot" 
         style={{cursor: "ne-resize", top: "0px",    right: "0px", transform: "translate(50%, -50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
-        className="resize-border resize-dot" 
+        className="resize-dot" 
         style={{cursor: "se-resize", bottom: "0px", right: "0px", transform: "translate(50%, 50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
-        className="resize-border resize-dot" 
+        className="resize-dot" 
         style={{cursor: "sw-resize", bottom: "0px", left: "0px",  transform: "translate(-50%, 50%)"}}></div>
 
       <div 
         ref={myContent}
-        onMouseDown={(e) => {onDrag(e)}}
+        idx={props.idx}
+        onMouseDown={(e) => {
+          if(isclicked === 'true'){
+            onDrag(e)
+          }
+        }}
         style={{
           width: "100%",
           height: "100%",
-          backgroundColor: "blue",
-          opacity: "0.3",
-        }}></div>
+          backgroundColor: "rgba(0, 0, 200, 0.3)",
+          color: "black",
+          fontSize: "36px",
+        }}>
+          {props.idx}
+      </div>
 
     </div>
   )
