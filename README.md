ec7f2ed46477add34dc0cbdda51b30e85a06a720
kuanhui.wu <kuanhui.wu@innolux.com>
2023-05-16 15:52:08 +0800

    update

--- README.md
+++ README.md
@@ -38,3 +38,7 @@ Backend will only respond built frontend in folder `build/`.
 To use node package like this way. 
 We can serve both frontend and backend on the same server.
 
+
+selection 
+一樣使用滑鼠位置引導方格大小變化，利用 absolute positioned element 的 top left right bottom 來作定位。
+不過，選擇框的四個點位會因為滑鼠位置變換而需要重新定義，或者說右下角的點會因為滑鼠位置不同而變成左上角，這樣一來四點需要做什麼處理？
--- src/CustomView.js
+++ src/CustomView.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react'
-import { AiOutlineFontSize, AiOutlineZoomIn, AiOutlineZoomOut } from 'react-icons/ai'
+import { AiOutlineZoomIn, AiOutlineZoomOut } from 'react-icons/ai'
 import './static/css/custom-view.css'
 
 const CustomView = () => {
@@ -9,7 +9,7 @@ const CustomView = () => {
   const container = useRef(null)
 
   useEffect(() => {
-    window.onclick = (e) => {   
+    window.onclick = (e) => { 
       setChoosedItem(e.target.getAttribute("idx"))
     };
   })
@@ -60,6 +60,7 @@ const CustomViewItem = (props) => {
   const myContainer = useRef(null)
   const myContent = useRef(null)
   const [isclicked, setIsclicked] = useState('false')
+  const [inEditMode, setInEditMode] = useState(false)
 
   useEffect(() => {
     if(props.idx === props.choosedItem){
@@ -86,6 +87,23 @@ const CustomViewItem = (props) => {
     }
   } 
 
+  const onTouchDrag = (e) => {
+    let pos11 = 0, pos12 = 0, pos21 = 0, pos22 = 0 
+    pos11 = e.touches[0].clientX
+    pos12 = e.touches[0].clientY
+    document.ontouchmove = (e) => {
+      pos21 = e.touches[0].clientX - pos11
+      pos22 = e.touches[0].clientY - pos12
+      pos11 = e.touches[0].clientX
+      pos12 = e.touches[0].clientY
+      myContainer.current.style.left = `${myContainer.current.offsetLeft + pos21}px`
+      myContainer.current.style.top  = `${myContainer.current.offsetTop  + pos22}px`
+    }
+    document.ontouchend = (e) => {
+      document.ontouchmove = null
+    }
+  }
+
   const onReWidth = (e) => {
     let pos1 = 0, pos2 = 0
     pos1 = e.clientX
@@ -97,6 +115,20 @@ const CustomViewItem = (props) => {
     } 
     document.onmouseup = () => {
       document.onmousemove = null
+    }
+  }
+
+  const onTouchReWidth = (e) => {
+    let pos1 = 0, pos2 = 0
+    pos1 = e.touches[0].clientX
+    // first position
+    document.ontouchmove = (e) => {
+      pos2 = e.touches[0].clientX - pos1 //movement
+      pos1 = e.touches[0].clientX
+      myContainer.current.style.width = `${myContainer.current.offsetWidth + pos2}px`
+    } 
+    document.ontouchend = () => {
+      document.ontouchmove = null
     }
   }
 
@@ -118,6 +150,25 @@ const CustomViewItem = (props) => {
     }
   } 
 
+  const onTouchResize = (e) => {
+    let pos11 = 0, pos12 = 0, pos21 = 0, pos22 = 0
+    pos11 = e.touches[0].clientX
+    pos12 = e.touches[0].clientY
+    // first position
+    document.ontouchmove = (e) => {
+      pos21 = e.touches[0].clientX - pos11 //movement
+      pos22 = e.touches[0].clientY - pos12 //movement
+      pos11 = e.touches[0].clientX
+      pos12 = e.touches[0].clientY
+      myContainer.current.style.width  = `${myContainer.current.offsetWidth  + pos21}px`
+      myContainer.current.style.height = `${myContainer.current.offsetHeight + pos22}px`
+    } 
+    document.ontouchend = () => {
+      document.ontouchmove = null
+    }
+  } 
+
+
 
   return(
     <div 
@@ -138,28 +189,34 @@ const CustomViewItem = (props) => {
 
       <div 
         onMouseDown={(e) => {onReWidth(e)}}
+        onTouchStart={(e) => {onTouchReWidth(e)}}
         className="resize-border" 
         style={{cursor: "w-resize", top: "50%",  left: "0px", height: "30px", width: "7px", borderRadius: "10px", transform: "translate(-100%, -50%)"}}></div>
 
       <div 
         onMouseDown={(e) => {onReWidth(e)}}
+        onTouchStart={(e) => {onTouchReWidth(e)}}
         className="resize-border" 
         style={{cursor: "e-resize", top: "50%", right: "0px", height: "30px", width: "7px", borderRadius: "10px", transform: "translate(100%, -50%)"}}></div>
 
       <div 
         onMouseDown={(e) => {onResize(e)}}
+        onTouchStart={(e) => {onTouchResize(e)}}
         className="resize-dot" 
         style={{cursor: "nw-resize", top: "0px", left: "0px", transform: "translate(-50%, -50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
+        onTouchStart={(e) => {onTouchResize(e)}}
         className="resize-dot" 
         style={{cursor: "ne-resize", top: "0px",    right: "0px", transform: "translate(50%, -50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
+        onTouchStart={(e) => {onTouchResize(e)}}
         className="resize-dot" 
         style={{cursor: "se-resize", bottom: "0px", right: "0px", transform: "translate(50%, 50%)"}}></div>
       <div 
         onMouseDown={(e) => {onResize(e)}}
+        onTouchStart={(e) => {onTouchResize(e)}}
         className="resize-dot" 
         style={{cursor: "sw-resize", bottom: "0px", left: "0px",  transform: "translate(-50%, 50%)"}}></div>
 
@@ -169,6 +226,11 @@ const CustomViewItem = (props) => {
         onMouseDown={(e) => {
           if(isclicked === 'true'){
             onDrag(e)
+          }
+        }}
+        onTouchStart={(e) => {
+          if(isclicked === 'true'){
+            onTouchDrag(e)
           }
         }}
         style={{
@@ -187,3 +249,129 @@ const CustomViewItem = (props) => {
 }
 
 export default CustomView
+
+
+// selection 
+
+function rectangleSelect(inputElements, selectionRectangle) {
+  var elements = [];
+  inputElements.forEach(function(element) {
+    var box = element.getBoundingClientRect();
+    if (
+      selectionRectangle.left <= box.left &&
+      selectionRectangle.top <= box.top &&
+      selectionRectangle.right >= box.right &&
+      selectionRectangle.bottom >= box.bottom
+    ) {
+      elements.push(element);
+    }
+  });
+  return elements;
+}
+
+function getSelectionRectNode() {
+  return document.querySelector(".selection-rect");
+}
+
+function showSelectionRectangle(selection) {
+  var rect = getSelectionRectNode();
+  rect.style.left = `${selection.left}px`;
+  rect.style.top = `${selection.top + window.scrollY}px`;
+  rect.style.width = `${selection.right - selection.left}px`;
+  rect.style.height = `${selection.bottom - selection.top}px`;
+  rect.style.opacity = 0.5;
+}
+
+function hideSelectionRectangle() {
+  var rect = getSelectionRectNode();
+  rect.style.opacity = 0;
+}
+
+function selectBoxes(selection) {
+  deselectBoxes();
+  rectangleSelect(getBoxes(), selection).forEach(function(box) {
+    box.classList.add("selected");
+  });
+}
+
+function deselectBoxes() {
+  getBoxes().forEach(function(box) {
+    box.classList.remove("selected");
+  });
+}
+
+function getBoxes() {
+  return [...document.querySelectorAll(".box")];
+}
+
+function initEventHandlers() {
+  var isMouseDown = false;
+  var selectionRectangle = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  };
+
+  function onMouseDown(e) {
+    isMouseDown = true;
+    deselectBoxes();
+    selectionRectangle.left = e.clientX;
+    selectionRectangle.top = e.clientY;
+  }
+
+  function onMouseMove(e) {
+    if (!isMouseDown) {
+      return;
+    }
+    selectionRectangle.right = e.clientX;
+    selectionRectangle.bottom = e.clientY;
+    showSelectionRectangle(selectionRectangle);
+    selectBoxes(selectionRectangle);
+  }
+
+  function onMouseUp(e) {
+    isMouseDown = false;
+    selectBoxes(selectionRectangle);
+    hideSelectionRectangle();
+    selectionRectangle = {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    };
+  }
+
+  document.addEventListener("mousedown", onMouseDown);
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+}
+
+// function initBoxes() {
+//   // Helpers for generating random boxes on screen
+//   function generateColumn(boxesPerColumn) {
+//     var node = document.createElement("div");
+//     node.className = "column";
+//     while (boxesPerColumn--) {
+//       var box = document.createElement("div");
+//       var sizeClassName = ["tiny", "small", "normal", "big", "huge"][
+//         Math.floor(Math.random() * 5)
+//       ];
+//       box.className = "box " + sizeClassName;
+//       node.appendChild(box);
+//     }
+//     return node;
+//   }
+//   function generateBoxes(parent, cols, boxesPerColumn) {
+//     while (cols--) {
+//       parent.appendChild(generateColumn(boxesPerColumn));
+//     }
+//   }
+
+//   generateBoxes(document.querySelector(".boxes"), 10, 10);
+// }
+
+function init() {
+  initEventHandlers();
+  // initBoxes();
+}
